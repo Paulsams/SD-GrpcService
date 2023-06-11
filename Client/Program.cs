7d6edcfb8a5088522bf48af6dbee83f184efec31
Paulsams <pasha.samsonov234@gmail.com>
2023-06-11 12:41:37 +0300

    Attempts to implement a single server and multiple clients

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,8 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using GrpcService;
 
-Console.Write("Enter your name: ");
-var clientName = Console.ReadLine();
+Console.WriteLine("Hello! \"Messenger\" welcomes you!");
 
 Console.Write("Enter your port: ");
 var port = Console.ReadLine();
@@ -10,11 +10,64 @@ var port = Console.ReadLine();
 using var channel = GrpcChannel.ForAddress($"http://localhost:{port}");
 var client = new User.UserClient(channel);
 
-while (true)
+var authorizationReply = await Authorization();
+var waiterMessagesCall = client.WaiterMessages();
+
+var waiterMessages = Task.Run(async () =>
 {
-    Console.Write("Enter message: ");
-    var message = Console.ReadLine();
+    await foreach (var response in waiterMessagesCall.ResponseStream.ReadAllAsync())
+        Console.WriteLine($"[{DateTime.Now}] <{response.UserName}>: {response.Text}");
+});
+
+await Task.Run(async () =>
+{
+    while (true)
+    {
+        var message = Console.ReadLine();
+        await waiterMessagesCall.RequestStream.WriteAsync(new MessageRequest()
+        {
+            UserId = authorizationReply.Id,
+            Text = message
+        });
+    }
+});
+
+async Task<AuthorizationReply> Authorization() {
+    while (true)
+    {
+        Console.Write("Enter your name: ");
+        var clientName = Console.ReadLine();
+
+        if (clientName == string.Empty)
+        {
+            Console.Error.WriteLine("The name cannot be empty");
+            continue;
+        }
     
-    var reply = await client.SendMessageAsync(new MessageRequest() { Text = message });
-    Console.WriteLine($"[{DateTime.Now}] <{clientName}>: {reply.Text}");
+        var reply = await client.AuthorizationAsync(new AuthorizationRequest()
+        {
+            Name = clientName
+        });
+
+        if (!reply.IsValid)
+        {
+            Console.Error.WriteLine("This name is already taken by another user");
+            continue;
+        }
+
+        Console.Write("Enter your password: ");
+        var password = Console.ReadLine();
+        var passwordReply = await client.SendPasswordAsync(new CheckPasswordRequest()
+        {
+            UserId = reply.Id,
+            // Давайте считать, что раскрытие паролей не важно). :sunglasses:
+            Password = password
+        });
+        
+        if (passwordReply.IsValid)
+            return reply;
+        
+        Console.Error.WriteLine("The password is incorrect");
+    }
 }
+
--- Server/Services/MessengerService.cs
+++ Server/Services/MessengerService.cs
@@ -4,6 +4,24 @@ namespace GrpcService.Services;
 
 public class MessengerService : User.UserBase
 {
+    private class UserInfo
+    {
+        public string Name { get; }
+        public string Password { get; }
+        
+        public UserInfo(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+    
+    // Я не понял прикола с тем, что сервис пересоздаётся, так что затычку в виде статики пока сделаю
+    private static readonly Dictionary<string, UserInfo> _users = new();
+    private static readonly Dictionary<string, string> _usersFromWaitPassword = new();
+    
+    private const string _userNameId = "Name";
+    
     private readonly ILogger<MessengerService> _logger;
 
     public MessengerService(ILogger<MessengerService> logger)
@@ -11,11 +29,56 @@ public class MessengerService : User.UserBase
         _logger = logger;
     }
 
-    public override Task<MessageReply> SendMessage(MessageRequest request, ServerCallContext context)
+    public override Task<AuthorizationReply> Authorization(AuthorizationRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new MessageReply
+        var userId = request.Name;
+        
+        var isNotValid = _usersFromWaitPassword.ContainsKey(userId) || _users.ContainsKey(userId);
+        if (!isNotValid)
+            _usersFromWaitPassword.Add(userId, request.Name);
+
+        return Task.FromResult(new AuthorizationReply()
         {
-            Text = request.Text
+            Id = isNotValid ? null : userId,
+            IsValid = !isNotValid
         });
+    }
+
+    public override Task<CheckPasswordReply> SendPassword(CheckPasswordRequest request, ServerCallContext context)
+    {
+        bool isValid;
+        if (_usersFromWaitPassword.TryGetValue(request.UserId, out var userName))
+        {
+            _usersFromWaitPassword.Remove(request.UserId);
+            _users.Add(request.UserId, new UserInfo(userName, request.Password));
+            isValid = true;
+        }
+        else
+        {
+            isValid = _users[request.UserId].Password == request.Password;
+        }
+        
+        return Task.FromResult(new CheckPasswordReply()
+        {
+            IsValid = isValid
+        });
+    }
+
+    public override async Task WaiterMessages(IAsyncStreamReader<MessageRequest> requestStream,
+        IServerStreamWriter<MessageReply> responseStream, ServerCallContext context)
+    {
+        var readTask = Task.Run(async () =>
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                await responseStream.WriteAsync(new MessageReply
+                {
+                    Text = message.Text,
+                    UserName = _users[message.UserId].Name
+                });
+            }
+        });
+        
+        await readTask;
     }
 }
